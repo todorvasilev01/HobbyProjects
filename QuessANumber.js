5660e730bb953cc09795769cb25865742c1eb09e
todorvasilev01 <123240012+todorvasilev01@users.noreply.github.com>
2023-01-22 11:52:03 +0100

    updated a whole code

--- QuessANumber.js
+++ QuessANumber.js
@@ -1,9 +1,29 @@
-function solve(num){
-    let CurrentNum=10;
-    let sum=0;
-    for(let i=num;i<=CurrentNum;i++){
-        sum+=i;
-    }
-    console.log(sum);
+
+function solve(){
+   const readLine=require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+   });
+   let computerQuess=Math.floor(Math.random()*100);
+   let quess;
+
+   let recursiveAsyncReadLine=function(){
+    readLine.question('Quess the number(0-100):',number => {
+        quess=Number(number);
+        if(quess <=100 && quess>=0){
+            if(quess==computerQuess){
+
+            }else if(quess<computerQuess){
+
+            }else if(quess>computerQuess){
+
+            }
+        }else{
+            console.log('Invalid input! Try again...');
+            recursiveAsyncReadLine();
+        }
+    })
+   }
+   recursiveAsyncReadLine();
 }
-solve(1)
+solve()
